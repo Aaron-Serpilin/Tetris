2d67fa5b8da81a8caf79c9ed9eab1d3e8bea52cc
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-09-26 16:18:37 +0200

    Horizontal Displacement of Blocks and Board limit Added

--- src/tetris/logic/TetrisLogic.scala
+++ src/tetris/logic/TetrisLogic.scala
@@ -34,24 +34,42 @@ class TetrisLogic(val randomGen: RandomGenerator, val gridDims: Dimensions, val 
       case TCell => currentBlock = TetrisBlock(TCell, List(Point(3, 1), Point(4, 1), Point(4, 0), Point(5, 1)), randomRotationIndex)
       case ZCell => currentBlock = TetrisBlock(ZCell, List(Point(3, 0), Point(4, 0), Point(4, 1), Point(5, 1)), randomRotationIndex)
     }
-    tetrisBlocks = currentBlock +: tetrisBlocks // We store all the blocks in a List
+
   }
 
   def rotateLeft(): Unit = ()
 
   def rotateRight(): Unit = ()
 
-  def moveLeft(): Unit = ()
+  def moveLeft(): Unit = {
+    val tetrisHorizontalBoardLimit = currentBlock.shape.map(_.x).min
+    if (tetrisHorizontalBoardLimit > 0) {
+      currentBlock = currentBlock.copy(shape = currentBlock.shape.map(point => point.copy(x = point.x - 1)))
+    }
+  }
 
-  def moveRight(): Unit = ()
+  def moveRight(): Unit = {
+    val tetrisHorizontalBoardLimit = currentBlock.shape.map(_.x).max
+    if (tetrisHorizontalBoardLimit + 1 < gridDims.width) {
+      currentBlock = currentBlock.copy(shape = currentBlock.shape.map(point => point.copy(x = point.x + 1)))
+    }
+  }
 
   def moveDown(): Unit = {
 
-    if (currentBlock == null) {
+    val tetrisBoardLimit = currentBlock.shape.map(_.y).max
+    if (tetrisBoardLimit + 1 < gridDims.height) {
+      currentBlock = currentBlock.copy(shape = currentBlock.shape.map(point => point.copy(y = point.y + 1)))
+    } else {
+      //Here the current block should remain at its final position, and then the current block should be reassigned to a new block which should be spawned
+      tetrisBlocks = currentBlock +: tetrisBlocks // We store all the blocks in a List once they've reached their final positions
       createBlock()
     }
 
-    currentBlock = currentBlock.copy(shape = currentBlock.shape.map(point => point.copy(y = point.y + 1)))
+//    for (blocks <- tetrisBlocks) {
+//      println(s"The blocks are ${blocks}")
+//    }
+
   }
 
   def doHardDrop(): Unit = ()
