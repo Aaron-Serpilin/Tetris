90e2235fa9790848a7a8215c494048218b54c926
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-09-26 14:05:42 +0200

    Random Generation of Tetris Blocks

--- src/tetris/logic/TetrisLogic.scala
+++ src/tetris/logic/TetrisLogic.scala
@@ -6,7 +6,7 @@ import tetris.logic.TetrisLogic._
 
 //case class GameState ()
 
-case class TetrisBlock(shape: CellType, blockPositions: List[Point], rotation: Int)
+case class TetrisBlock(color: CellType, shape: List[Point], rotation: Int)
 
 class TetrisLogic(val randomGen: RandomGenerator, val gridDims : Dimensions, val initialBoard: Seq[Seq[CellType]]) {
 
@@ -16,15 +16,25 @@ class TetrisLogic(val randomGen: RandomGenerator, val gridDims : Dimensions, val
   def this() =
     this(new ScalaRandomGen(), DefaultDims, makeEmptyBoard(DefaultDims))
 
-  private val blockTypes: Array[CellType] = Array(ICell, JCell, LCell, OCell, SCell, TCell, ZCell)
+  private val gameIsOver = false;
+  private val blockTypes: List[CellType] = List(ICell, JCell, LCell, OCell, SCell, TCell, ZCell)
   private var currentBlock: TetrisBlock = _
+  private val randomShapeIndex = randomGen.randomInt(blockTypes.length)
+  private val randomShape = blockTypes(randomShapeIndex)
 
-  def spawnBlock(): Unit = {
+  def createBlock(): Unit = {
     val numberPossibleRotations = 4
-    val randomShapeIndex = randomGen.randomInt(blockTypes.length)
     val randomRotationIndex = randomGen.randomInt(numberPossibleRotations)
-    val randomShape = blockTypes(randomShapeIndex)
-    currentBlock = TetrisBlock(randomShape, List(Point(3, 0), Point(4, 0), Point(5, 0), Point(6, 0)), rotation = randomRotationIndex)
+
+    randomShape match {
+      case ICell => currentBlock = TetrisBlock(ICell, List(Point(3, 1), Point(4, 1), Point(5, 1), Point(6, 1)), randomRotationIndex)
+      case JCell => currentBlock = TetrisBlock(JCell, List(Point(3, 0), Point(3, 1), Point(4, 1), Point(5, 1)), randomRotationIndex)
+      case LCell => currentBlock = TetrisBlock(LCell, List(Point(3, 1), Point(4, 1), Point(5, 1), Point(5, 0)), randomRotationIndex)
+      case OCell => currentBlock = TetrisBlock(OCell, List(Point(4, 0), Point(4, 1), Point(5, 0), Point(5, 1)), randomRotationIndex)
+      case SCell => currentBlock = TetrisBlock(SCell, List(Point(3, 1), Point(4, 1), Point(4, 0), Point(5, 0)), randomRotationIndex)
+      case TCell => currentBlock = TetrisBlock(TCell, List(Point(3, 1), Point(4, 1), Point(4, 0), Point(5, 1)), randomRotationIndex)
+      case ZCell => currentBlock = TetrisBlock(ZCell, List(Point(3, 0), Point(4, 0), Point(4, 1), Point(5, 1)), randomRotationIndex)
+    }
   }
 
   def rotateLeft(): Unit = ()
@@ -35,25 +45,30 @@ class TetrisLogic(val randomGen: RandomGenerator, val gridDims : Dimensions, val
 
   def moveRight(): Unit = ()
 
-  def moveDown(): Unit = ()
+  def moveDown(): Unit = {
+//    for (blockPoint <- currentBlock.shape) {
+//      blockPoint.y -= 1
+//    }
+  }
 
   def doHardDrop(): Unit = ()
 
-  def isGameOver: Boolean = false
+  def isGameOver: Boolean = gameIsOver
 
   def getCellType(p : Point): CellType = {
-    spawnBlock()
-    if (currentBlock.blockPositions.contains(p)) {
-      ICell
+    createBlock()
+    if (currentBlock.shape.contains(p)) {
+      currentBlock.color
     } else {
       Empty
     }
   }
+
 }
 
 object TetrisLogic {
 
-  val FramesPerSecond: Int = 10
+  val FramesPerSecond: Int = 1/100
   val DrawSizeFactor = 1.0
 
   def makeEmptyBoard(gridDims : Dimensions): Seq[Seq[CellType]] = {
